d5b97cac5ff3b27bac8c8b55516b8b4b949aa8f5
Ubuntu <ubuntu@ip-172-26-0-156.ec2.internal>
2025-12-04 04:52:56 +0000

    Adding all results except PT to github for bimodal

--- Figure2/plot_w.R
+++ Figure2/plot_w.R
@@ -49,5 +49,11 @@ for (prior in c("gaussian","skew","cauchy","bimodal")) {
   fname = sprintf("plots/VI_%s.pdf",prior)
   prefix = sprintf("results/VI_%s_block02corr0.9_nbyp1.0_invSNR%.1f_lambda%.3f",prior,noiser,lambda)
   plot.w(fname,prefix,Theta,w.true,"green",title,seeded=FALSE)
+  print("  PolyaTree")
+  title = springf("Polya Tree")
+  fname = sprintf("plots/PolyaTree_%s.pdf", prior)
+  prefix = sprintf("results/PolyaTree_%s_block02corr0.9_nbyp1.0_invSNR%.1f", prior, noiser)
+  plot.w(fname, prefix, Theta, w.true, "green", title, seeded=FALSE)
+
 }
 
